600526f39a0581e33875c5438f68cddd6270c066
Daniel Barfuss <46683580+DanielBarfuss@users.noreply.github.com>
2019-01-31 10:18:27 -0700

    Update Main.java

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -3,12 +3,7 @@ public class Main {
 
 	public static void main(String args[])
 	{
-<<<<<<< HEAD
 		System.out.println("test test");
-		
-		
-=======
 		System.out.println("Duck");
->>>>>>> branch 'master' of https://github.com/breadthatisfrench/GroupRepository.git
 	}
 }
