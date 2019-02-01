525f236597db81c2ddf7885e2abaa56f39ee5e9b
absptab <absptab@CCSD-45DXBH2.ccsdut.net>
2019-02-01 10:01:13 -0700

    Added more support to main

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.JFrame;
+
 import dflt.MyScreen;
 
 public class Main {
@@ -5,6 +7,9 @@ public class Main {
 	public static void main(String args[])
 	{
 		MyScreen ms = new MyScreen();
+		JFrame frame = new JFrame("Name");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		frame.add(ms.getPanel());
 	}
 }
--- Project/src/dflt/MyScreen.java
+++ Project/src/dflt/MyScreen.java
@@ -1,13 +1,21 @@
 package dflt;
 
-import java.awt.Color;
+
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
+import javax.swing.JPanel;
+
 import bensAbstract.Screen;
 
-public class MyScreen extends Screen {
+public class MyScreen extends Screen 
+{
 
+	
+	public JPanel getPanel()
+	{
+		return panel;
+	}
 	
 	public MyScreen()
 	{
@@ -19,11 +27,6 @@ public class MyScreen extends Screen {
 	protected void update() {
 		pixels = new BufferedImage(panel.getWidth(), panel.getHeight(), pixels.TYPE_INT_RGB);
 		Graphics2D g = (Graphics2D)pixels.getGraphics();
-		
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 	}
 
