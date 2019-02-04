9b1987a6507b8e399747b2d58ec36c7e377521f2
ABSPTAB <ABSPTAB@CCSD-45DXBH2.ccsdut.net>
2019-02-04 10:16:07 -0700

    streamlined JFrame

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -6,11 +6,11 @@ public class Main {
 
 	public static void main(String[] args)
 	{
-		MyScreen ms = new MyScreen();
+		//MyScreen ms = new MyScreen();
 		JFrame.setDefaultLookAndFeelDecorated(true);
 		JFrame frame = new JFrame("Window");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		frame.pack();
 		frame.setVisible(true);
 	}
